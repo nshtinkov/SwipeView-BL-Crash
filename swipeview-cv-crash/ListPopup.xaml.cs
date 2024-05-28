5e5836028b8493ed3815c982c7fe2e529a52b9c3
Nikolay Shtinkov <nshtinkov@gmail.com>
2024-05-28 11:02:40 -0400

    Simplified code

--- swipeview-cv-crash/ListPopup.xaml.cs
+++ swipeview-cv-crash/ListPopup.xaml.cs
@@ -4,12 +4,8 @@ namespace swipeview_cv_crash;
 
 public partial class ListPopup : Popup
 {
-	public List<string> Items { get; set; } = ["One"];
-
 	public ListPopup()
 	{
 		InitializeComponent();
-
-		BindingContext = this;
 	}
 }
